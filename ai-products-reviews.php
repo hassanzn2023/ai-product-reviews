0dfb050bf8e303d3208a12735aa74b5d49f0248d
hassanzn2023 <164942270+hassanzn2023@users.noreply.github.com>
2024-09-06 18:03:37 +0300

    Update ai-products-reviews.php

--- ai-products-reviews.php
+++ ai-products-reviews.php
@@ -3,7 +3,7 @@
 Plugin Name: AI Product Reviews
 Plugin URI: https://github.com/hassanzn2023/ai-product-reviews
 Description: Automatically generates product reviews using AI by fetching product title and description.
-Version: 2.1.2
+Version: 2.2.2
 Author: Hassan Zein
 Author URI: http://skillyweb.com
 Text Domain: ai-reviews
